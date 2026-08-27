88063d8d80076949ac6994676a7df40f847c1fec
thecoachmanuel <thecoachmanuel@users.noreply.github.com>
2026-08-27 18:26:19 +0100

    Set outputDirectory to public and increase chunkSizeWarningLimit

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        chunkSizeWarningLimit: 1600,
+    },
     server: {
         watch: {
             ignored: [
